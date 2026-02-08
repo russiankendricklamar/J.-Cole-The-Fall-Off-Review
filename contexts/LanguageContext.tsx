0deafe38d29b15e3b735a745fb31f84b002faf13
Egor Galkin <lifeofwhitedesiigner@gmail.com>
2026-02-08 19:31:08 +0300

    Update LanguageContext.tsx

--- contexts/LanguageContext.tsx
+++ contexts/LanguageContext.tsx
@@ -171,7 +171,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export const LanguageProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [language, setLanguage] = useState<Language>('ru'); 
+  const [language, setLanguage] = useState<Language>('en'); 
 
   useEffect(() => {
     document.documentElement.lang = language;
