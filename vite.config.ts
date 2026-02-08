349e27af7a2e4f50a003f7a27b8a463b85928483
Egor Galkin <lifeofwhitedesiigner@gmail.com>
2026-02-08 13:01:10 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: '/J.-Cole-The-Fall-Off-Review/',  // ← ключевая строка
   server: {
     port: 3000,
     host: '0.0.0.0',
