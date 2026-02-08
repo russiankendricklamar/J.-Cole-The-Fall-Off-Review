198e6cd3ac238bbda955d2fa053701575dc12e95
Egor Galkin <lifeofwhitedesiigner@gmail.com>
2026-02-08 16:45:56 +0300

    Update Footer.tsx

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
           <h2 className="font-anton text-[15vw] md:text-[8vw] leading-[0.8] text-red-600 uppercase">
             J. Cole
           </h2>
-          <p className="font-oswald text-xl uppercase mt-2">The Fall Off</p>
+          <p className="font-oswald text-xl uppercase mt-2">"The Fall-Off" album</p>
         </div>
 
         <div className="grid grid-cols-2 gap-12 font-mono text-xs text-neutral-500 uppercase">
@@ -20,8 +20,7 @@ const Footer: React.FC = () => {
           </div>
           <div>
             <h4 className="text-white mb-4">Design</h4>
-            <p>Ref: Studio</p>
-            <p>Dev: React/Tailwind</p>
+            <p>Production: "Не до конца рандомные строчки из песен"</p>
             <p>Year: 2025</p>
           </div>
         </div>
